79a4a1b41ba18a63ea57ae09c9cecf99fd356fd1
dave <daveexinor@cmail.carleton.ca>
2025-10-24 02:25:01 -0400

    lab6: created home page
    Signed-off-by: dave <daveexinor@cmail.carleton.ca>

--- src/main/java/org/example/AddressBookViewController.java
+++ src/main/java/org/example/AddressBookViewController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.View;
 
 import java.util.Optional;
@@ -26,5 +27,12 @@ public class AddressBookViewController {
             return "addressbook-view"; // Thymeleaf template name
         }
         return "error";
+    }
+
+    // Display home page of Address Book web application
+    @PostMapping("/home")
+    public String viewHomePage(Model model) {
+        model.addAttribute("addressBooks", addressBookRepository.findAll());
+        return "home";
     }
 }
